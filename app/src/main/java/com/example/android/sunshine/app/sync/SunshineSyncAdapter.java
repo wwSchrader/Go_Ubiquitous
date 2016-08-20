6e6969733f5358455b358ff802dba0c58a44f2e5
Warren Schrader <wwschrader@gmail.com>
2016-08-19 20:59:04 -0700

    Temperature data is now synced with app.

--- app/src/main/java/com/example/android/sunshine/app/sync/SunshineSyncAdapter.java
+++ app/src/main/java/com/example/android/sunshine/app/sync/SunshineSyncAdapter.java
@@ -58,7 +58,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Date;
 import java.util.Vector;
 import java.util.concurrent.ExecutionException;
 
@@ -461,7 +460,7 @@ public class SunshineSyncAdapter extends AbstractThreadedSyncAdapter implements 
             PutDataMapRequest putDataMapReq = PutDataMapRequest.create("/weather");
             putDataMapReq.getDataMap().putString("temp-high", tempHigh);
             putDataMapReq.getDataMap().putString("temp-low", tempLow);
-            putDataMapReq.getDataMap().putLong("time", new Date().getTime());
+            putDataMapReq.getDataMap().putLong("time", System.currentTimeMillis());
 
             PutDataRequest putDataRequest = putDataMapReq.asPutDataRequest();
             Wearable.DataApi.putDataItem(mGoogleApiClient, putDataRequest)
